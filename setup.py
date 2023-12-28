d9eb4369759169edb26c4990a2969d74fb410e50
RicardoAlegreMiranda <alegrmiranda@gmail.com>
2023-12-28 20:33:08 +0100

    0.802 add test

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="ouigo-py",
+    version="1.0.0",
+    description="A module which allows you to retrieve data about the cheapest one-way travels of Ouigo "
+    "in a date range.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Ricardo Alegre",
+    author_email="https://www.linkedin.com/in/ricardo-alegre-miranda/",
+    url="https://github.com/RicardoAlegreMiranda/ouigo-py",
+    packages=["ouigo"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["requests"]
+)
